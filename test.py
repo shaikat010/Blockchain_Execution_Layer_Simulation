269f51de15b88cb9ded9905c70ba599115f61bd5
Shaikat Majumder <68814937+shaikat010@users.noreply.github.com>
2024-05-04 02:44:10 +0600

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from create_keys_signatures import *
-data = "erverververvtrv"
-node_private_key = get_private_signing_key()
-print(node_private_key)
-print("***************************************************")
-signature = make_signature(data,node_private_key)
-print("***********This is the signature ***************")
-print(signature)
